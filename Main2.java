b667a67198707fcff86a7a3056c215aafd8c9d52
Luis Rey Lara Gonzalez <48841140+luisreylaragonzalez@users.noreply.github.com>
2021-02-17 13:27:32 -0600

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,18 @@
+
+package main2;
+
+/**
+ *
+ * @author luisreylarag
+ */
+public class Main2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola UPJR y Guanajuato");
+        System.out.println("17Feb2021 github new");
+    }
+    
+}
